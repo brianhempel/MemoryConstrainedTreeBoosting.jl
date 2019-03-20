e47f5502569787be0b8112cfb267de3fbb0d984e
Brian Hempel <bnhempel@mtu.edu>
2019-03-20 11:49:14 -0500

    Modified profiling setup (used to assess row-major version)

--- test/Profile.jl
+++ test/Profile.jl
@@ -7,8 +7,8 @@ using Profile
 
 Random.seed!(123456)
 
-feature_count = 150
-point_count   = 12_000_000
+feature_count = 1500
+point_count   = 1_300_000
 
 X       = randn(Float32, (point_count, feature_count))
 y       = round.(rand(MemoryConstrainedTreeBoosting.Prediction, point_count))
@@ -17,8 +17,7 @@ weights = rand(MemoryConstrainedTreeBoosting.DataWeight, point_count)
 
 bin_splits = prepare_bin_splits(X)
 X_binned   = apply_bins(X, bin_splits)
-# X_binned   = bin_and_compress(X, bin_splits)
-# X_binned   = finalize_loading(X_binned_compressed)
+X          = nothing
 
 trees = train_on_binned(X_binned, y, weights = weights, iteration_count = 2, feature_fraction = 0.5, max_leaves = 6, bagging_temperature = 0.5)
 
